b45303a9d4dd2f3d78e5769206072ad5ac188a65
Ivan Hribernik <cihribernik@gmail.com>
2023-07-20 20:55:15 -0300

    added startup task for mongo db and constants

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-from .routers import authentication, movies, shows
+from app.routers import authentication, movies, shows
+from app.settings.database import init
 
 app = FastAPI()
 
 app.include_router(authentication.router)
 app.include_router(movies.router)
 app.include_router(shows.router)
+
+
+@app.on_event("startup")
+async def start_db():
+    await init()
